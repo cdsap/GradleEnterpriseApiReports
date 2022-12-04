4003f5b4da8dde62a041e53fb60fa580866e0466
Inaki Villar <inaki.seri@gmail.com>
2022-12-04 18:28:02 +0100

    checking 0 value

--- geapi/src/main/java/io/github/cdsap/geapi/domain/model/Build.kt
+++ geapi/src/main/java/io/github/cdsap/geapi/domain/model/Build.kt
@@ -28,18 +28,26 @@ class Measurement(
     fun diff(): Any {
         if (variantA is Int) {
             if ((variantA as Int) - (variantB as Int) != 0) {
-                val x = (variantB * 100) / variantA
-                val result = 100 - x
-                return "$result%"
+                if(variantA != 0) {
+                    val x = (variantB * 100) / variantA
+                    val result = 100 - x
+                    return "$result%"
+                } else {
+                    return ""
+                }
             } else {
                 return ""
             }
 
         } else if (variantA is Long) {
             if ((variantA as Long) - (variantB as Long) != 0L) {
-                val x = (variantB * 100L) / variantA
-                val result = 100L - x
-                return "$result%"
+                if(variantA != 0L) {
+                    val x = (variantB * 100L) / variantA
+                    val result = 100L - x
+                    return "$result%"
+                } else{
+                    return ""
+                }
             } else {
                 return ""
             }
